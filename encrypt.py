bee115a2a990436708f7780fe4b7e7dff30ac05e
kuiqejw <lauraong1@yahoo.com.sg>
2019-04-03 13:23:45 +0800

    Delete encrypt.py

--- encrypt.py
+++
@@ -1,9 +0,0 @@
-import pyAesCrypt
-# This uses AES256-CBC to decrypt files and binary streams. 
-# encryption/decryption buffer size - 64K
-bufferSize = 64 * 1024
-password = "foopassword"
-# encrypt
-pyAesCrypt.encryptFile("plaintext.pdf", "encrypted.pdf", password, bufferSize)
-# decrypt
-pyAesCrypt.decryptFile("encrypted.pdf", "decrypted.pdf", password, bufferSize)
